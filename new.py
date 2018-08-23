ece788a3fd0b4637a7981fe1a4edf060cc9fba0d
speecher <hit_master@163.com>
2018-08-23 22:49:09 +0800

    complete generator

--- new.py
+++ new.py
@@ -3,13 +3,19 @@ import numpy as np
 import scipy.io.wavfile as wav
 from collections import Counter
 from python_speech_features import mfcc
-
+from keras.models import Model
+from keras.layers import Dense, Dropout, Input, Reshape 
+from keras.layers import Conv1D,LSTM,MaxPooling1D, Lambda, TimeDistributed, Activation,Conv2D, MaxPooling2D
+from keras.layers.merge import add, concatenate
+from keras import backend as K
+from keras.optimizers import SGD, Adadelta
+from keras.layers.recurrent import GRU
+from keras.preprocessing.sequence import pad_sequences
 
 def genwavlist(wavpath):
 	wavfiles = {}
 	fileids = []
 	for (dirpath, dirnames, filenames) in os.walk(wavpath):
-		print(dirpath)
 		for filename in filenames:
 			if filename.endswith('.wav'):
 				filepath = os.sep.join([dirpath, filename])
@@ -18,10 +24,15 @@ def genwavlist(wavpath):
 				fileids.append(fileid)
 	return wavfiles,fileids
 
+
 def compute_mfcc(file):
 	fs, audio = wav.read(file)
-	mfcc_feat = mfcc(audio, samplerate=fs, numcep=13)
+	mfcc_feat = mfcc(audio, samplerate=fs, numcep=26)
+	mfcc_feat = mfcc_feat[::3]
+	mfcc_feat = np.transpose(mfcc_feat)  
+	mfcc_feat = pad_sequences(mfcc_feat, maxlen=500, dtype='float', padding='post', truncating='post').T
 	return mfcc_feat
+
 
 def gendict(textfile_path):
 	dicts = []
@@ -36,6 +47,7 @@ def gendict(textfile_path):
 	wordsize = len(words)
 	word2num = dict(zip(words, range(wordsize)))
 	return word2num,len(word2num)
+
 
 def text2num(textfile_path):
 	lexcion,wordnum = gendict(textfile_path)
@@ -53,7 +65,8 @@ def text2num(textfile_path):
 		content_dict[cont_id] = content
 	return content_dict,lexcion
 
-def get_batch(x, y, train=False, max_pred_len=4, input_length=8):
+
+def get_batch(x, y, train=False, max_pred_len=50, input_length=500):
     X = np.expand_dims(x, axis=3)
     X = x # for model2
 #     labels = np.ones((y.shape[0], max_pred_len)) *  -1 # 3 # , dtype=np.uint8
@@ -71,8 +84,9 @@ def get_batch(x, y, train=False, max_pred_len=4, input_length=8):
     return (inputs, outputs)
 
 
-def data_generate(wavpath = 'E:\\Data\\data_thchs30\\dev', textfile = 'E:\\Data\\thchs30\\cv.syllable.txt', bath_size=32):
+def data_generate(wavpath = 'D:\\workspace\\github\\data', textfile = 'D:\\workspace\\github\\data\\test.txt', bath_size=1):
 	wavdict,fileids = genwavlist(wavpath)
+	print(wavdict)
 	content_dict,lexcion = text2num(textfile)
 	genloop = len(fileids)//bath_size
 	for i in range(genloop):
@@ -85,15 +99,51 @@ def data_generate(wavpath = 'E:\\Data\\data_thchs30\\dev', textfile = 'E:\\Data\
 			mfcc_feat = compute_mfcc(wavdict[fileid])
 			feats.append(mfcc_feat)
 			labels.append(content_dict[fileid])
-		yield feats,labels
+		feats = np.array(feats)
+		labels = np.array(labels)
+		print(np.shape(feats))
+		print(np.shape(labels))
+		inputs, outputs = get_batch(feats, labels)
+		yield inputs, outputs
 
 
+def ctc_lambda(args):
+	labels, y_pred, input_length, label_length = args
+	y_pred = y_pred[:, :, :]
+	return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
 
+def creatModel():
+	input_data = Input(name='the_input', shape=(500, 26))
+	layer_h1 = Dense(512, activation="relu", use_bias=True, kernel_initializer='he_normal')(input_data)
+	layer_h1 = Dropout(0.3)(layer_h1)
+	layer_h2 = Dense(512, activation="relu", use_bias=True, kernel_initializer='he_normal')(layer_h1)
+	layer_h3_1 = GRU(512, return_sequences=True, kernel_initializer='he_normal', dropout=0.3)(layer_h2) # GRU
+	layer_h3_2 = GRU(512, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', dropout=0.3)(layer_h2) # GRU
+	layer_h3 = add([layer_h3_1, layer_h3_2])
+	layer_h4 = Dense(512, activation="relu", use_bias=True, kernel_initializer='he_normal')(layer_h3)
+	layer_h4 = Dropout(0.3)(layer_h4)
+	layer_h5 = Dense(1200, activation="relu", use_bias=True, kernel_initializer='he_normal')(layer_h4)
+	output = Activation('softmax', name='Activation0')(layer_h5)
+	model_data = Model(inputs=input_data, outputs=output)
+	#ctc
+	labels = Input(name='the_labels', shape=[50], dtype='float32')
+	input_length = Input(name='input_length', shape=[1], dtype='int64')
+	label_length = Input(name='label_length', shape=[1], dtype='int64')
+	loss_out = Lambda(ctc_lambda, output_shape=(1,), name='ctc')([labels, output, input_length, label_length])
+	model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_out)
+	model.summary()
+	ada_d = Adadelta(lr=0.01, rho=0.95, epsilon=1e-06)
+	#model=multi_gpu_model(model,gpus=2)
+	model.compile(loss={'ctc': lambda y_true, output: output}, optimizer=ada_d)
+	#test_func = K.function([input_data], [output])
+	print("model compiled successful!")
+	return model, model_data
 
 
-yields = data_generate()
-for i,j in yields:
-	print(np.shape(i))
-	print(np.shape(j))
+model, model_data = creatModel()
+yielddatas = data_generate()
+model.fit_generator(yielddatas,1)
+model.save_weights('model.mdl')
+model_data.save_weights('model_data.mdl')
 #text2num('E:\\Data\\thchs30\\cv.syllable.txt')
